3816df70a53bf14991e1e60ce507f1d6145b7e08
Joochan <joochan123123@gmail.com>
2025-04-19 02:12:09 +0900

    feat: LightComponent Type 추가.

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/SceneTypes.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "LightDefine.h"
+
+enum ELightComponentType
+{
+    LightType_Directional = LIGHT_TYPE_DIRECTIONAL,
+    LightType_Point		  = LIGHT_TYPE_POINT,
+    LightType_Spot		  = LIGHT_TYPE_SPOT,
+    LightType_Rect 		  = LIGHT_TYPE_RECT,
+    LightType_MAX         = LIGHT_TYPE_MAX,
+    LightType_NumBits = 2
+};
