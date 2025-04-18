b419746a5f91f11434e9d4cccf209f0af0f7d895
UISOO <zeroapple000@gmail.com>
2025-04-18 21:49:52 +0900

    Add Destroy, Select Component (#1)
    * Add Destroy, Select Component
    
    * Fix Select Component

--- EngineSIU/EngineSIU/Engine/Source/Editor/PropertyEditor/PropertyEditorPanel.h
+++ EngineSIU/EngineSIU/Engine/Source/Editor/PropertyEditor/PropertyEditorPanel.h
@@ -4,6 +4,15 @@
 #include "UnrealEd/EditorPanel.h"
 #include "Math/Rotator.h"
 
+class USpotLightComponent;
+class UDirectionalLightComponent;
+class UPointLightComponent;
+class UAmbientLightComponent;
+class UProjectileMovementComponent;
+class UTextComponent;
+class UHeightFogComponent;
+class AEditorPlayer;
+class USceneComponent;
 class UStaticMeshComponent;
 
 // 헬퍼 함수 예시
@@ -37,19 +46,33 @@ private:
     void RGBToHSV(float r, float g, float b, float& h, float& s, float& v) const;
     void HSVToRGB(float h, float s, float v, float& r, float& g, float& b) const;
 
+    void RenderForSceneComponent(USceneComponent* SceneComponent, AEditorPlayer* Player) const;
+    void RenderForActor(AActor* SelectedActor, USceneComponent* TargetComponent) const;
+
     /* Static Mesh Settings */
     void RenderForStaticMesh(UStaticMeshComponent* StaticMeshComp) const;
+
+    void RenderForAmbientLightComponent(UAmbientLightComponent* LightComp) const;
+    void RenderForDirectionalLightComponent(UDirectionalLightComponent* LightComponent) const;
+    void RenderForPointLightComponent(UPointLightComponent* LightComponent) const;
+    void RenderForSpotLightComponent(USpotLightComponent* LightComponent) const;
+
+    
+    void RenderForProjectileMovementComponent(UProjectileMovementComponent* ProjectileComp) const;
+    void RenderForTextComponent(UTextComponent* TextComponent) const;
     
     /* Materials Settings */
     void RenderForMaterial(UStaticMeshComponent* StaticMeshComp);
     void RenderMaterialView(UMaterial* Material);
     void RenderCreateMaterialView();
+
+    void RenderForExponentialHeightFogComponent(UHeightFogComponent* ExponentialHeightFogComp) const;
+
+    template<typename T>
+    requires std::derived_from<T, UActorComponent>
+T* GetTargetComponent(AActor* SelectedActor, USceneComponent* SelectedComponent);
 private:
     float Width = 0, Height = 0;
-    FVector Location = FVector(0, 0, 0);
-    FRotator Rotation = FRotator(0, 0, 0);
-    FVector Scale = FVector(0, 0, 0);
-    FVector LightDirection = FVector(0, 0, 0);
     /* Material Property */
     int SelectedMaterialIndex = -1;
     int CurMaterialIndex = -1;
