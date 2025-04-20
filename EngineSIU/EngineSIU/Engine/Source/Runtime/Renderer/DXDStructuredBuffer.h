67fa7b16a0fc3feba565f81041305c7f3ad09626
Joochan <joochan123123@gmail.com>
2025-04-21 02:54:11 +0900

    feat: StructuredBuffer 구조체 추가.

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/DXDStructuredBuffer.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "HAL/PlatformType.h"
+#include "Container/Array.h"
+#include "UserInterface/Console.h"
+
+class ID3D11Device;
+class ID3D11Buffer;
+class ID3D11ShaderResourceView;
+
+struct FDXDStructuredBuffer
+{
+    
+public:
+    FDXDStructuredBuffer() : Buffer(nullptr), SRV(nullptr), UAV(nullptr), ElementCount(0), ElementStride(0) {}
+
+    // StructuredBuffer 데이터.
+    ID3D11Buffer* Buffer = nullptr;
+    // StructuredBuffer에 대한 SRV.
+    ID3D11ShaderResourceView* SRV;
+    ID3D11UnorderedAccessView* UAV;
+    UINT ElementCount; // StructuredBuffer의 Element 개수.
+    UINT ElementStride; // 각 Element 별 크기.
+};
