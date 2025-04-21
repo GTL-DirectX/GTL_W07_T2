7d020ec1e463ff46fd30798acde7b623b5c2cf0b
tang-ka <gur0907@hanyang.ac.kr>
2025-04-21 21:08:11 +0900

    SpotLight Cone 수정

--- EngineSIU/EngineSIU/Shaders/StaticMeshPixelShader.hlsl
+++ EngineSIU/EngineSIU/Shaders/StaticMeshPixelShader.hlsl
@@ -121,7 +121,7 @@ float4 mainPS(PS_INPUT_StaticMesh Input) : SV_Target
         FinalColor += float4(0.01, 0.01, 0.0, 1);
     }
     
-    FinalColor *= ShadowMapLight;
+    //FinalColor *= ShadowMapLight;
     
     return FinalColor;
 }
