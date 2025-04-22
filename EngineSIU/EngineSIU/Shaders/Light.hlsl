ad17e4063c6189a5dab97b9bbdb7092d726a9526
tang-ka <gur0907@hanyang.ac.kr>
2025-04-22 16:55:20 +0900

    BugFix: SpotLight cone 버그 수정

--- EngineSIU/EngineSIU/Shaders/Light.hlsl
+++ EngineSIU/EngineSIU/Shaders/Light.hlsl
@@ -230,13 +230,7 @@ float GetLightFromShadowMap(float3 WorldPosition, uint LightIndex)
     //    0.5f - (LightClipSpacePos.y / LightClipSpacePos.w) / 2.f
     //};
     
-    //bool IsInX = ShadowMapTexCoord.x < 0 || ShadowMapTexCoord.x > 1;
-    //bool IsInY = ShadowMapTexCoord.y < 0 || ShadowMapTexCoord.y > 1;
     
-    //float LightDistance = LightClipSpacePos.z / LightClipSpacePos.w;
-    
-    //if (IsInX || IsInY || LightDistance > 1)
-    //    return 1.0f;
     
     //float NdotL = dot(normalize(WorldNormal), normalize(LightDirection));
     //float TotalBias = max(BiasStep * (1.0 - NdotL), MinBias);
@@ -293,15 +287,20 @@ float GetLightFromShadowMap(float3 WorldPosition, uint LightIndex)
         };
         float LightDistance = LightClipSpacePos.z / LightClipSpacePos.w;
         //LightDistance -= bias;
+        
+        bool IsInX = ShadowMapTexCoord.x < 0 || ShadowMapTexCoord.x > 1;
+        bool IsInY = ShadowMapTexCoord.y < 0 || ShadowMapTexCoord.y > 1;
+    
+        if (IsInX || IsInY || LightDistance > 1)
+            return 1.0f;
 
-
-        if (bIsDirectional)
-        {
-            Result = DirectionalShadowMap.SampleCmpLevelZero(ShadowSampler, float3(ShadowMapTexCoord, TargetIndex), LightDistance).r;
-        }
-        else if (bIsSpot)
+        if (bIsSpot)
         {
             Result = SpotShadowMap.SampleCmpLevelZero(ShadowSampler, float3(ShadowMapTexCoord, TargetIndex), LightDistance).r;
+        }
+        else if (bIsDirectional)
+        {
+            Result = DirectionalShadowMap.SampleCmpLevelZero(ShadowSampler, float3(ShadowMapTexCoord, TargetIndex), LightDistance).r;
         }
     }
     
@@ -313,6 +312,7 @@ float3 Lighting(float3 WorldPosition, float3 WorldNormal, float3 WorldViewPositi
     ShadowMapLight = 0;
     uint ShadowMapLightCount = 0;
 
+    float3 LightColor = float3(0.0, 0.0, 0.0);
     float3 FinalColor = float3(0.0, 0.0, 0.0);
 
     uint LightIndex = 0;
@@ -320,25 +320,37 @@ float3 Lighting(float3 WorldPosition, float3 WorldNormal, float3 WorldViewPositi
     // Light (Dir -> Point -> Spot)순서 바뀌면 위험함.
     for (int k = 0; k < DirectionalLightsCount; k++)
     {
-        FinalColor += DirectionalLight(k, WorldPosition, WorldNormal, WorldViewPosition, DiffuseColor, SpecularColor, Shininess);
+        LightColor = DirectionalLight(k, WorldPosition, WorldNormal, WorldViewPosition, DiffuseColor, SpecularColor, Shininess);
         ShadowMapLight += GetLightFromShadowMap(WorldPosition, LightIndex);
-        ShadowMapLightCount++;
+        
+        LightColor *= ShadowMapLight;
+        FinalColor += LightColor;
+        
+        //ShadowMapLightCount++;
         LightIndex++;
     }
     
     for (int i = 0; i < PointLightsCount; i++)
     {
-        FinalColor += PointLight(i, WorldPosition, WorldNormal, WorldViewPosition, DiffuseColor, SpecularColor, Shininess);
+        LightColor = PointLight(i, WorldPosition, WorldNormal, WorldViewPosition, DiffuseColor, SpecularColor, Shininess);
         ShadowMapLight += GetLightFromShadowMap(WorldPosition, LightIndex);
-        ShadowMapLightCount++;
+        
+        LightColor *= ShadowMapLight;
+        FinalColor += LightColor;
+        
+        //ShadowMapLightCount++;
         LightIndex++;
     }    
 
     for (int j = 0; j < SpotLightsCount; j++)
     {
-        FinalColor += SpotLight(j, WorldPosition, WorldNormal, WorldViewPosition, DiffuseColor, SpecularColor, Shininess);
+        LightColor = SpotLight(j, WorldPosition, WorldNormal, WorldViewPosition, DiffuseColor, SpecularColor, Shininess);
         ShadowMapLight += GetLightFromShadowMap(WorldPosition, LightIndex);
-        ShadowMapLightCount++;
+        
+        LightColor *= ShadowMapLight;
+        FinalColor += LightColor;
+        
+        //ShadowMapLightCount++;
         LightIndex++;
     }
 
@@ -347,15 +359,14 @@ float3 Lighting(float3 WorldPosition, float3 WorldNormal, float3 WorldViewPositi
         FinalColor += AmbientLights[l].AmbientColor.rgb * DiffuseColor;
     }
 
-    if (ShadowMapLightCount > 0)
-    {
-        ShadowMapLight /= ShadowMapLightCount;
-    }
-    else
-    {
-        ShadowMapLight = 1;
-    }
-    
+    //if (ShadowMapLightCount > 0)
+    //{
+    //    ShadowMapLight /= ShadowMapLightCount;
+    //}
+    //else
+    //{
+    //    ShadowMapLight = 1;
+    //}
     
     return FinalColor;
 }
