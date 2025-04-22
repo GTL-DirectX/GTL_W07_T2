9a800e6ed25beffc34278867cf7b22eb0eaa0c62
Joochan <joochan123123@gmail.com>
2025-04-22 15:54:27 +0900

    fix: Shadow 타입 이동.

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/Types/ShadowTypes.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/Types/ShadowTypes.h
@@ -8,3 +8,11 @@ struct FShadowLightConstants
     float FarPlane;
     uint32 PointLightIndex;
 };
+
+struct FShadowInfo
+{
+    float ShadowResolutionScale; // 그림자 해상도 비율
+    float ShadowBias; // 그림자 바이어스
+    float ShadowSlopeBias; // 그림자 경사 바이어스
+    float ShadowSharpen; // 그림자 선명도
+};
